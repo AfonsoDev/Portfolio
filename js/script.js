2e9b7fb6a5cce624c5017bb1b7985625530f5792
AfonsoDev <peresfon@gmail.com>
2022-10-02 13:08:56 -0300

    novos icons
    add icons de dev

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict"
+
+$( document ).ready(function() {
+    const html = 0;
+    const css = 0;
+    const javascript = 0;
+    const nodejs = 0;
+    const bootstrap = 0;
+    const tailwind = 0;
+    const reactjs = 0;
+    const reactnative = 0;
+    const python = 0;
+
+    console.log( "ready!" );
+});
